fa24e36dd2ca64a1464996ca11106ad8065cbf84
Shohei Aoki <shoaok@gmail.com>
2019-11-15 08:43:13 +0300

    add postprocessor and result

--- main.m
+++ main.m
@@ -1,3 +1,4 @@
+clear all; close all;
 [NODES,ELEMENTS,COMPONENTS,NODES_TRIA3,...
     DOF_NODE,DOF_TOTAL,DOF_TRIA3,THICKNESS,...
     YOUNG,POISSON,x,y,cnnc] = init_fem();
@@ -8,6 +9,7 @@ K=make_K(Ke,ELEMENTS,DOF_TRIA3,DOF_NODE,cnnc,NODES);
 [Kc,F,U,Um]=set_BC(DOF_TOTAL,K);
 U2=Kc\F;
 U2
+postprocess(x,y,U2);
 %Fr=make_reaction(K,U,DOF_TOTAL)
 %strain_element=make_strain_element(ELEMENTS,NODES_TRIA3,DOF_TRIA3,COMPONENTS,B,cnnc,U);
 %make_stress_element();
---
+++ readme.md
@@ -0,0 +1,20 @@
+# FEM simulation for 2D plain strain problem (triangular finite element)
+```MATLAB
+>> main
+U2 =
+
+         0
+         0
+   -0.0010
+   -0.0031
+   -0.0012
+   -0.0067
+    0.0015
+   -0.0068
+    0.0014
+   -0.0029
+         0
+   -0.0011
+```
+
+![](result.png)
