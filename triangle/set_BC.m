778f18feb13c7543224f84bbfa55288666bfc412
Shohei Aoki <shoaok@gmail.com>
2025-04-11 16:39:07 +0300

    update

--- triangle/set_BC.m
+++ triangle/set_BC.m
@@ -7,8 +7,9 @@ Um = zeros(DOF_TOTAL,1);
 U(1) = 0;U(2) = 0;U(3) = 0;U(4) = 0;U(5) = 0;U(6) = 0;
 U(7) = 0;U(8) = 0;U(9) = 0;U(10) = 0;U(11) = 0;U(12) = 0;
 
+Fmax = -100;
 F(1) = 0;F(2) = 0;F(3) = 0;F(4) = 0;F(5) = 0;F(6) = 0;
-F(7) = 0;F(8) = -100;F(9) = 0;F(10) = 0;F(11) = 0;F(12) = 0;
+F(7) = 0;F(8) = Fmax;F(9) = 0;F(10) = 0;F(11) = 0;F(12) = 0;
 
 Um(1) = true;Um(2) = true;Um(3) = false;Um(4) = false;Um(5) = false;Um(6) = false;
 Um(7) = false;Um(8) = false;Um(9) = false;Um(10) = false;Um(11) = true;Um(12) = false;
